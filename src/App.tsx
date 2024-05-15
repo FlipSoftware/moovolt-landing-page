96d919da963a977cecb5878f760821834d38afb9
amindWalker <bhrochamail@gmail.com>
2024-05-15 16:14:04 -0300

    `style`: add new hero with the Moov.olt HUD

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,8 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "@/components/ui/tooltip";
 
-import {
-    CircleWidget,
-    CircleWidgetFallback,
-} from "@/components/hud/CircleWidget";
+import { CircleWidget } from "@/components/hud/CircleWidget";
 
 export default function App() {
     function getCurrentYear(): number {
@@ -29,19 +20,20 @@ export default function App() {
     return (
         <div className="flex flex-col min-h-screen bg-gradient-linear bg-gradient-from-black bg-gradient-to-blueGray-9">
             <div className="grid">
-                <nav className="sticky top-0 z-10 w-full bg-foreground bg-op-10 backdrop-blur-lg drop-shadow-xl">
+                <nav className="sticky top-0 z-10 w-full bg-background bg-op-50 backdrop-blur-lg">
                     <div className="flex-center h-[60px] px-4">
                         <div className="flex-center space-x-4">
                             <a
                                 href="#"
-                                className="flex-center space-x-2 hover:text-yellow-3 text-xl"
+                                className="flex-center space-x-2 brightness-50 hover:(brightness-100 drop-shadow-xl drop-shadow-color-yellow-2 drop-shadow-color-op-10) focus:(brightness-100 drop-shadow-xl drop-shadow-color-yellow-2 drop-shadow-color-op-10)"
                             >
-                                <span className="i-mage:light-bulb-electricity mb-1 text-2xl" />
-                                <span className="font-semibold text-fore <md:hidden">
-                                    MOOV.OLT
-                                </span>
+                                <img
+                                    src="/moovolt-name.svg"
+                                    alt="moovolt-name"
+                                    className="max-h-10"
+                                />
                             </a>
-                            <div className="grid grid-flow-col gap-4 grid-place-items-center text-xl">
+                            <div className="grid grid-flow-col gap-4 grid-place-items-center text-2xl <md:text-lg">
                                 <a href="#">Soluções</a>
                                 <a href="#">Preços</a>
                                 <a href="#">Sobre</a>
@@ -50,38 +42,113 @@ export default function App() {
                     </div>
                 </nav>
                 <main className="px-32 <md:px-8 w-screen <md:-mt-8 overflow-hidden">
-                    <img
+                    {/* <img
                         src="/ev-charger-hud.jpg"
                         alt="ev-charger-hud"
                         className="absolute z-0 place-self-center left-70 top-0 op-10"
-                    />
-                    <div className="relative grid grid-flow-col grid-cols-center z-1">
-                        <img
-                            className="min-h-10 min-w-10 max-h-xl max-w-xl col-end-1"
-                            src="/moovolt-logo.png"
-                            alt="moovolt-logo"
-                        />
-                        <div className="hud grid grid-cols-12 gap-8">
-                            <TooltipProvider>
-                                <Tooltip>
-                                    <TooltipTrigger>
-                                        <CircleWidget
-                                            className="w-24 h-24 border grid grid-place-items-center hover:brightness-150 bg-gradient-to-b col-end-1 active:scale-90"
-                                        >
-                                            <CircleWidgetFallback className="i-fluent:battery-checkmark-20-filled w-8 h-8 hover:bg-green" />
-                                        </CircleWidget>
-                                    </TooltipTrigger>
-                                    <TooltipContent>
-                                        <p>Status da Bateria</p>
-                                    </TooltipContent>
-                                </Tooltip>
-                            </TooltipProvider>
+                    /> */}
+                    <div className="relative z-1 grid grid-flow-col grid-cols-center grid-items-start rounded-full border-b border-l border-r backdrop-blur-64 p-32">
+                        <div>
+                            <div className="hud grid gap-16 grid-cols-[200px,repeat(auto-fill,minmax(15%,100px)),300px]">
+                                <div className="grid gap-8 grid-place-content-center">
+                                    <CircleWidget className="w-24 h-24 border-t grid grid-place-items-center hover:brightness-150 bg-gradient-to-b active:scale-90" />
 
-                            <CircleWidget className="w-24 h-24 border grid grid-place-items-center hover:brightness-150 bg-gradient-to-b col-end-1">
-                                <CircleWidgetFallback className="i-gis:location-poi-o w-8 h-8 hover:bg-orange-6" />
-                            </CircleWidget>
+                                    <CircleWidget className="w-24 h-24 border-t grid grid-place-items-center hover:brightness-150 bg-gradient-to-b active:scale-90" />
+
+                                    <CircleWidget className="w-24 h-24 border-t grid grid-place-items-center hover:brightness-150 bg-gradient-to-b active:scale-90" />
+
+                                    <CircleWidget className="w-24 h-24 border-t grid grid-place-items-center hover:brightness-150 bg-gradient-to-b active:scale-90" />
+                                </div>
+
+                                <CircleWidget
+                                    tabIndex={0}
+                                    className="relative border-t border-l w-xl h-xl <md:(w-md h-md) grid place-content-center rounded-full drop-shadow-2xl drop-shadow-color-muted drop-shadow-color-op-50"
+                                >
+                                    <hr className="absolute circular-charging inset-12 w-10 h-10 outline outline-muted -outline-offset-1 drop-shadow-2xl drop-shadow-color-teal" />
+                                    <img
+                                        src="/moovolt-car-custom-internal.svg"
+                                        alt="moovolt-car-custom-interal"
+                                        className="absolute top-40 right-15 max-w-md <md:(max-w-sm) col-start-2 brightness-60"
+                                    />
+                                    <img
+                                        src="/moovolt-car-custom-body.svg"
+                                        alt="moovolt-car-custom-interal"
+                                        className="absolute top-40 right-15 max-w-md <md:(max-w-sm) col-start-2 hover:(animate-pulse) focus:(animate-pulse) brightness-60"
+                                        tabIndex={0}
+                                    />
+                                </CircleWidget>
+
+                                <div className="col-start-3">
+                                    {/* Reserved */}
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <section className="w-full py-12 md:py-24 lg:py-32 bg-gradient-to-t bg-gradient-from-black bg-gradient-to-background -mt-32 rounded-lg">
+                        <div className="grid mt-16 items-center gap-4 px-4 text-center md:px-6 lg:gap-10">
+                            <div className="space-y-2">
+                                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                                    Soluções diversas adequadas ao cliente
+                                </h2>
+                                <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                                    De condomínios até grandes corporações.
+                                </p>
+                            </div>
+                            <div className="mx-auto grid max-w-5xl items-start gap-6 py-12 lg:grid-cols-2 lg:gap-12">
+                                <img
+                                    alt="Image"
+                                    className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last op-50"
+                                    height="310"
+                                    src="/placeholder.svg"
+                                    width="550"
+                                />
+
+                                <div className="flex flex-col justify-center space-y-4">
+                                    <ul>
+                                        <li>
+                                            <div className="grid gap-1">
+                                                <h3 className="text-xl font-bold">
+                                                    Vídeo Promocional
+                                                </h3>
+                                                <p className="text-sm text-gray-500 dark:text-gray-400">
+                                                    Entenda mais
+                                                </p>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="w-full py-12 md:py-24 lg:py-32">
+                        <Card className="p-4">
+                            <h2 className="text-3xl font-bold m-8 ml-4 sm:text-4xl md:text-5xl">
+                                MOOVOLT
+                            </h2>
+                            <hr />
+                            <div className="grid grid-auto-flow-col <md:grid-auto-flow-row">
+                                <p className="m-4 text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                                    é uma solução voltada para a utilização
+                                    inteligente de espaços urbanos. Ela
+                                    interliga o carregamento inteligente de
+                                    veículos elétricos ou híbridos plug-in a
+                                    soluções de carregamento em estações urbanas
+                                    e em estradas. Faz um mapeamento que
+                                    sinaliza onde há estações disponíveis para o
+                                    usuário, permite reserva, fornece
+                                    informações de recarga e facilita o
+                                    pagamento.
+                                </p>
+                                <div className="m-8 <md:m-4">
+                                    <img
+                                        alt="EV Charger"
+                                        className="rounded object-cover op-50"
+                                        src="https://images.unsplash.com/photo-1593941707882-a5bba14938c7?q=80&w=1744&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                    />
+                                </div>
+                            </div>
+                        </Card>
+                    </section>
                     <section className="w-full py-12 md:py-24 lg:py-32">
                         <div className="grid grid-items-center grid-justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
                             <div className="space-y-3">
@@ -223,42 +290,7 @@ export default function App() {
                             </div>
                         </div>
                     </section>
-                    <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
-                        <div className="grid items-center gap-4 px-4 text-center md:px-6 lg:gap-10">
-                            <div className="space-y-2">
-                                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-                                    Soluções diversas adequadas ao cliente
-                                </h2>
-                                <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                                    De condomínios até grandes corporações.
-                                </p>
-                            </div>
-                            <div className="mx-auto grid max-w-5xl items-start gap-6 py-12 lg:grid-cols-2 lg:gap-12">
-                                <img
-                                    alt="Image"
-                                    className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last op-50"
-                                    height="310"
-                                    src="/placeholder.svg"
-                                    width="550"
-                                />
 
-                                <div className="flex flex-col justify-center space-y-4">
-                                    <ul>
-                                        <li>
-                                            <div className="grid gap-1">
-                                                <h3 className="text-xl font-bold">
-                                                    Vídeo Promocional
-                                                </h3>
-                                                <p className="text-sm text-gray-500 dark:text-gray-400">
-                                                    Entenda mais
-                                                </p>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
                     <section className="w-full py-12 md:py-24 lg:py-32 border-t">
                         <div className="px-4 md:px-6">
                             <div className="grid gap-10 px-10 md:gap-16 lg:grid-cols-2">
